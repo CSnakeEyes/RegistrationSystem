85dda584549241ba707adebd868a1ad0c6034d6d
Cristian Ayub Chavira <CSnakeEyes@users.noreply.github.com>
2016-12-12 00:42:13 -0700

    Update RegisterStudent.java

--- RegisterStudent.java
+++ RegisterStudent.java
@@ -1,6 +1,7 @@
 package regPrototype;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashSet;
@@ -48,54 +50,60 @@ public class RegisterStudent extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 102, 153));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblRegisterANew = new JLabel("Register a new student");
+		lblRegisterANew.setForeground(new Color(255, 255, 255));
 		lblRegisterANew.setBounds(150, 11, 112, 14);
 		contentPane.add(lblRegisterANew);
 		
 		JTextPane txtpnPleaseEnterThe = new JTextPane();
 		txtpnPleaseEnterThe.setText("Please enter the following information");
-		txtpnPleaseEnterThe.setBounds(111, 23, 187, 20);
+		txtpnPleaseEnterThe.setBounds(111, 36, 187, 20);
 		contentPane.add(txtpnPleaseEnterThe);
 		
 		txtFirstName = new JTextField();
 		txtFirstName.setToolTipText("First Name");
-		txtFirstName.setBounds(10, 79, 86, 20);
+		txtFirstName.setBounds(10, 108, 86, 20);
 		contentPane.add(txtFirstName);
 		txtFirstName.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("First Name");
-		lblNewLabel.setBounds(10, 54, 58, 14);
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(10, 82, 86, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblLastName = new JLabel("Last Name");
-		lblLastName.setBounds(10, 110, 58, 14);
+		lblLastName.setForeground(new Color(255, 255, 255));
+		lblLastName.setBounds(10, 139, 74, 14);
 		contentPane.add(lblLastName);
 		
 		txtLastName = new JTextField();
 		txtLastName.setToolTipText("Last Name");
-		txtLastName.setBounds(10, 135, 86, 20);
+		txtLastName.setBounds(10, 164, 86, 20);
 		contentPane.add(txtLastName);
 		txtLastName.setColumns(10);
 		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(115, 54, 58, 14);
+		lblUsername.setForeground(new Color(255, 255, 255));
+		lblUsername.setBounds(150, 82, 81, 14);
 		contentPane.add(lblUsername);
 		
 		textUsername = new JTextField();
-		textUsername.setBounds(115, 79, 86, 20);
+		textUsername.setBounds(150, 108, 86, 20);
 		contentPane.add(textUsername);
 		textUsername.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(115, 110, 46, 14);
+		lblPassword.setForeground(new Color(255, 255, 255));
+		lblPassword.setBounds(153, 139, 67, 14);
 		contentPane.add(lblPassword);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(115, 135, 86, 20);
+		passwordField.setBounds(154, 164, 86, 20);
 		contentPane.add(passwordField);
 		
 		JButton btnConfirm = new JButton("Confirm");
